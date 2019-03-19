fe08f7cb45d63f417610a8bb8be8bf497fcf159c
Arnaud Christ <arnaud.christ@gmail.com>
2019-03-19 21:28:22 +0100

    Add sites.json and basic listing in home page.

---
+++ atoms/site-card.js
@@ -0,0 +1,3 @@
+const SiteCard = ({ title, description, site_url }) => <div>{title}</div>;
+
+export default SiteCard;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import { Fragment } from "react";
 import Layout from "../components/layout";
 import Landing from "../components/landing";
+import sites from "../resources/sites";
+import Title from "../atoms/title";
+import SiteCard from "../atoms/site-card";
 
 const Index = () => (
   <Layout>
     <Landing />
-    Home page
+    {sites.map((category, index) => (
+      <Fragment key={index}>
+        <Title text={category.category_name} />
+        {category.sites.map((site, key) => (
+          <SiteCard key={index} title={site.title} />
+        ))}
+      </Fragment>
+    ))}
   </Layout>
 );
 
